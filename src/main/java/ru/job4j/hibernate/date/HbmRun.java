d7fce700b8e16eb582bfc9b769b3bafac59b875b
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-04-13 15:00:17 +0600

    some commit

---
+++ src/main/java/ru/job4j/hibernate/date/HbmRun.java
@@ -0,0 +1,33 @@
+package ru.job4j.hibernate.date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class HbmRun {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            List<Product> products = session.createQuery("from Product").list();
+            for (Product pr : products) {
+                System.out.println(pr);
+            }
+
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/hibernate/date/Product.java
@@ -0,0 +1,41 @@
+package ru.job4j.hibernate.date;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String producer;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    public static Product of(String name, String producer) {
+        Product p = new Product();
+        p.name = name;
+        p.producer = producer;
+        p.created = new Date(System.currentTimeMillis());
+        return p;
+    }
+
+    // getters and setters
+    // equals and hashcode
+
+    @Override
+    public String toString() {
+        return "Product{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", producer='" + producer + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
