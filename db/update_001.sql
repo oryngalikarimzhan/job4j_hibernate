4eb2b284b5572ce89fda9cadd8fc614dec714b32
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-04-17 01:10:49 +0600

    0. Что такое интеграционное тестирование. [#6875]

---
+++ db/update_001.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS orders (
+    id serial,
+    name VARCHAR(50),
+    description VARCHAR(50),
+    created timestamp,
+    PRIMARY KEY (id)
+);
---
+++ src/test/java/ru/job4j/integration/OrdersStoreTest.java
@@ -0,0 +1,93 @@
+package ru.job4j.integration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class OrdersStoreTest {
+    private BasicDataSource pool = new BasicDataSource();
+
+    @Before
+    public void setUp() throws SQLException {
+        pool.setDriverClassName("org.hsqldb.jdbcDriver");
+        pool.setUrl("jdbc:hsqldb:mem:tests;sql.syntax_pgs=true");
+        pool.setUsername("sa");
+        pool.setPassword("");
+        pool.setMaxTotal(2);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream("./db/update_001.sql")))
+        ) {
+            br.lines().forEach(line -> builder.append(line).append(System.lineSeparator()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pool.getConnection().prepareStatement(builder.toString()).executeUpdate();
+    }
+
+    @After
+    public void rollback() throws SQLException {
+        pool.setDriverClassName("org.hsqldb.jdbcDriver");
+        pool.setUrl("jdbc:hsqldb:mem:tests;sql.syntax_pgs=true");
+        pool.setUsername("sa");
+        pool.setPassword("");
+        pool.setMaxTotal(2);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream("./db/rollback_001.sql")))
+        ) {
+            br.lines().forEach(line -> builder.append(line).append(System.lineSeparator()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pool.getConnection().prepareStatement(builder.toString()).executeUpdate();
+    }
+
+    @Test
+    public void whenUpdateOrderAndFindAllOneRowWithNewDescription() {
+        OrdersStore store = new OrdersStore(pool);
+        Order savedOrder = store.save(Order.of("name1", "description1"));
+        savedOrder.setDescription("description2");
+        Order updatedOrder = store.update(savedOrder);
+
+        List<Order> all = (List<Order>) store.findAll();
+
+        assertThat(all.size(), is(1));
+        assertThat(all.get(0).getDescription(), is(updatedOrder.getDescription()));
+        assertThat(all.get(0).getId(), is(1));
+    }
+
+    @Test
+    public void whenSaveOrderAndFindAllOneRowWithDescription() {
+        OrdersStore store = new OrdersStore(pool);
+        store.save(Order.of("name1", "description1"));
+
+        List<Order> all = (List<Order>) store.findAll();
+
+        assertThat(all.size(), is(1));
+        assertThat(all.get(0).getDescription(), is("description1"));
+        assertThat(all.get(0).getId(), is(1));
+    }
+
+    @Test
+    public void whenDeleteOrderAndFindAllSizeZero() {
+        OrdersStore store = new OrdersStore(pool);
+
+        Order order = store.save(Order.of("name1", "description1"));
+        store.delete(order);
+        List<Order> all = (List<Order>) store.findAll();
+
+        assertThat(all.size(), is(0));
+    }
+}
