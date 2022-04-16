a1ac88304c35d3688022c33f49093675a9bae401
oryngalikarimzhan <oryngalikarimzhan@github.com>
2022-04-16 18:39:14 +0600

    0. HQL [#6874 #288393]

--- src/main/java/ru/job4j/hql/HbmRun.java
+++ src/main/java/ru/job4j/hql/HbmRun.java
@@ -14,44 +14,11 @@ public class HbmRun {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
             Session session = sf.openSession();
             session.beginTransaction();
-            /*Vacancy firstVacancy = Vacancy.of(
-                    "senior Java",
-                    "we need senior Java developer",
-                    5000,
-                    "ex@hh.ru",
-                    "87088602830"
-            );
-            Vacancy secondVacancy = Vacancy.of(
-                    "senior Kotlin",
-                    "we need senior Kotlin developer",
-                    5000,
-                    "ex@hh.ru",
-                    "87088602830"
-            );
-            Vacancy thirdVacancy = Vacancy.of(
-                    "senior Java",
-                    "we need senior Java developer",
-                    6000,
-                    "xe@hh.ru",
-                    "87087602830"
-            );
-            session.save(firstVacancy);
-            session.save(secondVacancy);
-            session.save(thirdVacancy);
-            VacancyBase vacancyBase = VacancyBase.of("senior Java");
-            vacancyBase.addVacancy(firstVacancy);
-            vacancyBase.addVacancy(thirdVacancy);
-            Candidate firstCandidate = Candidate.of("Stas", 5, 250000, vacancyBase);
-            session.save(firstCandidate);*/
 
-            System.out.println(session.createQuery("select distinct c from Candidate c "
-                    + "join fetch c.vacancyBase vb "
-                    + "join fetch vb.vacancyList "
-                    + "where c.id = :id", Candidate.class)
-                    .setParameter("id", 1)
-                    .uniqueResult()
-            );
-
+            session.createQuery("from Candidate where name = :name")
+                    .setParameter("name", "Petr")
+                    .list()
+                    .forEach(System.out::println);
             session.getTransaction().commit();
             session.close();
         }  catch (Exception e) {
